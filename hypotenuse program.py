3134923e7a0af8d99a18a48c182549ace494257c
Shumba Makhado <makhadoshumba@gmail.com>
2026-08-17 13:38:05 +0200

    programs

--- hypotenuse program.py
+++ hypotenuse program.py
@@ -1,2 +1,10 @@
 import math
 
+print("Welcome to the hypotenuse program")
+print("")
+print("*******************************************************")
+print("")
+a = input("Give me a: ")
+b = input("give me b: ")
+
+c = input
