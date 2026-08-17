b16689133f7388e56bfa21c14ddd0a5069a0e5e5
Shumba Makhado <makhadoshumba@gmail.com>
2026-08-17 13:31:25 +0200

    programs

---
+++ area program.py
@@ -0,0 +1,58 @@
+print("Welcome To The Area program")
+print("")
+print("Choose your favoured program:")
+print("")
+print("1. Area of a square")
+print("2. Area of a triangle")
+print("3. Area of a rectangle")
+print("4. Area of a circle")
+print("")
+
+programs=int(input("Choose your program: "))
+
+def switches(programs):
+    match programs:
+        case 1:
+            print("We are finding the Area of a square right now")
+            print("")
+            side= input("Please enter the length of one side so that we can calculate the area: ")
+            side=int(side)
+            square_area = side*side
+            print("")
+            print(f"The area of your square is {square_area}")
+            return
+        case 2:
+            print("We are finding the Area of a triangle right now")
+            print("")
+            base= input("Please enter the base of the triangle: ")
+            height= input("Please enter the height of the triangle: ")
+            base=int(base)
+            height=int(height)
+            triangle_area = 0.5 * base * height
+            print("")
+            print(f"The area of your triangle is {triangle_area}")
+            return 
+        case 3:
+            print("We are finding the Area of a rectangle right now")
+            print("")
+            width= input("Enter the width of the rectangle: ")
+            height= input("Enter the height of the rectangle: ")
+            width= int(width)
+            height= int(height)
+            rectangle_area = width * height
+            print("")
+            print(f"The area of your rectangle is {rectangle_area}")
+            return
+        case 4:
+            print("We are finding the Area of a circle right now")
+            print("")
+            radius= input("Enter the radius of the circle: ")
+            radius=int(radius)
+            circle_area = 3.14 * (radius**2)
+            print("")
+            print(f"The area of your circle is {circle_area}")
+            return
+        case _:
+            print("Invalid program selected.")
+
+switches(programs)            
