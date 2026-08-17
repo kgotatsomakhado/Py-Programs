afd7f8923956c131f1f51d13bd1a795a7a78095f
Shumba Makhado <makhadoshumba@gmail.com>
2026-08-17 14:05:31 +0200

    programs

--- area program.py
+++ area program.py
@@ -19,17 +19,25 @@ print("*******************************************************")
 def switches(programs):
     match programs:
         case 1:
-            print("We are finding the Area of a square right now")
+            print("*******************************************************")
+            print("")
+            print("     We are finding the Area of a square right now")
+            print("")
+            print("*******************************************************")
             print("")
             side= input("Please enter the length of one side so that we can calculate the area: ")
             side=int(side)
             square_area = side*side
             print("")
             print(f"The area of your square is {square_area}")
+            print("")
+            print("*******************************************************")
             return
         case 2:
+            print("*******************************************************")
             print("We are finding the Area of a triangle right now")
             print("")
+            print("*******************************************************")
             base= input("Please enter the base of the triangle: ")
             height= input("Please enter the height of the triangle: ")
             base=int(base)
@@ -37,9 +45,14 @@ def switches(programs):
             triangle_area = 0.5 * base * height
             print("")
             print(f"The area of your triangle is {triangle_area}")
+            print("*******************************************************")
             return 
         case 3:
+            print("*******************************************************")
+            print("")
             print("We are finding the Area of a rectangle right now")
+            print("")
+            print("*******************************************************")
             print("")
             width= input("Enter the width of the rectangle: ")
             height= input("Enter the height of the rectangle: ")
@@ -48,15 +61,23 @@ def switches(programs):
             rectangle_area = width * height
             print("")
             print(f"The area of your rectangle is {rectangle_area}")
+            print("")
+            print("*******************************************************")
             return
         case 4:
+            print("*******************************************************")
+            print("")
             print("We are finding the Area of a circle right now")
+            print("")
+            print("*******************************************************")
             print("")
             radius= input("Enter the radius of the circle: ")
             radius=int(radius)
             circle_area = 3.14 * (radius**2)
             print("")
             print(f"The area of your circle is {circle_area}")
+            print("")
+            print("*******************************************************")
             return
         case _:
             print("Invalid program selected.")
