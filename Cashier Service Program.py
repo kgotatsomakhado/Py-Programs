76e6ddaceaef1716507680abe36bb445acbd2354
Shumba Makhado <makhadoshumba@gmail.com>
2026-08-18 10:59:16 +0200

    prgrms

--- Cashier Service Program.py
+++ Cashier Service Program.py
@@ -1,3 +1,14 @@
+#Products Prices
+
+Burger_and_chips = 49.66
+Pizza = 99.99
+Burger = 32.99
+Chips = 27.50
+Cool_drink = 10
+Ice_cream = 5
+
+#Program
+
 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 print("")
 print("   Welcome To The Cashier Program")
@@ -11,4 +22,12 @@ print("2. Pizza")
 print("3. Burger Only")
 print("4. Chips Only")
 print("5. Cool Drink")
+print("6. Ice cream")
 print("")
+product= int(input("What did the customer buy?: "))
+print("")
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+if product == 1:
+    price = int(input("How much did the customer pay for the Burger & Chips: "))
+    
